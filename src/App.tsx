203d65ddec2bd8ce2b5008256378c444f4025c15
Fabio Costa <fscosta114@gmail.com>
2023-09-11 15:56:56 -0300

    iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+    return (
+        <SafeAreaView>
+         <Text>Bora aprender react</Text>
+        </SafeAreaView>
+    );
+
+};
+
+// eslint-disable-next-line prettier/prettier
+export default App;
